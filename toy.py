f4414ff31df4a751487cadee284a20303eb633ac
doehyeonlee <anthony16@snu.ac.kr>
2025-10-02 14:49:06 +0000

    toy update

--- toy.py
+++ toy.py
@@ -1,5 +1,11 @@
-import diffusers, transformers, torch
-print("diffusers:", diffusers.__version__)
-print("transformers:", transformers.__version__)
-print("torch:", torch.__version__)
-print("cuda available:", torch.cuda.is_available())
+import h5py
+
+filename = "/root/dataset/rec/gJS_sFM_cAll_d02_mJS5_ch10.h5"
+with h5py.File(filename, "r") as f:
+    # 최상위 키들 보기
+    print("Keys:", list(f.keys()))
+
+    # 특정 데이터셋 shape, dtype 확인
+    for k in f.keys():
+        dset = f[k]
+        print(f"{k}: shape={dset.shape}, dtype={dset.dtype}")
